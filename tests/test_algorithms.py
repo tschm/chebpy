18dfd9d64fc1ae80fb2ab40a659e0ce22df2d982
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-26 13:51:29 +0200

    test_algorithms

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,7 +1,6 @@
 """Unit-tests for pyfun/utilities.py"""
 
-import unittest
-
+import pytest
 import numpy as np
 
 from chebpy.core.settings import DefaultPreferences
@@ -24,47 +23,57 @@ np.random.seed(0)
 np.seterr(divide="ignore", invalid="ignore")
 
 
-class Evaluation(unittest.TestCase):
-    """Tests for the Barycentric formula and Clenshaw algorithm"""
+@pytest.fixture
+def evaluation_data():
+    npts = 15
+    return {
+        "xk": Chebtech2._chebpts(npts),
+        "vk": Chebtech2._barywts(npts),
+        "fk": np.random.rand(npts),
+        "ak": np.random.rand(11),
+        "xx": -1 + 2 * np.random.rand(9),
+        "pts": -1 + 2 * np.random.rand(1001)
+    }
 
-    def setUp(self):
-        npts = 15
-        self.xk = Chebtech2._chebpts(npts)
-        self.vk = Chebtech2._barywts(npts)
-        self.fk = np.random.rand(npts)
-        self.ak = np.random.rand(11)
-        self.xx = -1 + 2 * np.random.rand(9)
-        self.pts = -1 + 2 * np.random.rand(1001)
-
+# Tests for the Barycentric formula and Clenshaw algorithm
+class TestEvaluation:
     # check an empty array is returned whenever either or both of the first
     # two arguments are themselves empty arrays
-    def test_bary__empty(self):
+    def test_bary__empty(self, evaluation_data):
+        pts = evaluation_data["pts"]
         null = (None, None)
-        self.assertEquals(bary(np.array([]), np.array([]), *null).size, 0)
-        self.assertEquals(bary(np.array([0.1]), np.array([]), *null).size, 0)
-        self.assertEquals(bary(np.array([]), np.array([0.1]), *null).size, 0)
-        self.assertEquals(bary(self.pts, np.array([]), *null).size, 0)
-        self.assertEquals(bary(np.array([]), self.pts, *null).size, 0)
-        self.assertNotEquals(bary(np.array([0.1]), np.array([0.1]), *null).size, 0)
+        assert bary(np.array([]), np.array([]), *null).size == 0
+        assert bary(np.array([0.1]), np.array([]), *null).size == 0
+        assert bary(np.array([]), np.array([0.1]), *null).size == 0
+        assert bary(pts, np.array([]), *null).size == 0
+        assert bary(np.array([]), pts, *null).size == 0
+        assert bary(np.array([0.1]), np.array([0.1]), *null).size != 0
 
-    def test_clenshaw__empty(self):
-        self.assertEquals(clenshaw(np.array([]), np.array([])).size, 0)
-        self.assertEquals(clenshaw(np.array([]), np.array([1.0])).size, 0)
-        self.assertEquals(clenshaw(np.array([1.0]), np.array([])).size, 0)
-        self.assertEquals(clenshaw(self.pts, np.array([])).size, 0)
-        self.assertEquals(clenshaw(np.array([]), self.pts).size, 0)
-        self.assertNotEquals(clenshaw(np.array([0.1]), np.array([0.1])).size, 0)
+    def test_clenshaw__empty(self, evaluation_data):
+        pts = evaluation_data["pts"]
+        assert clenshaw(np.array([]), np.array([])).size == 0
+        assert clenshaw(np.array([]), np.array([1.0])).size == 0
+        assert clenshaw(np.array([1.0]), np.array([])).size == 0
+        assert clenshaw(pts, np.array([])).size == 0
+        assert clenshaw(np.array([]), pts).size == 0
+        assert clenshaw(np.array([0.1]), np.array([0.1])).size != 0
 
     # check that scalars get evaluated to scalars (not arrays)
-    def test_clenshaw__scalar_input(self):
-        for x in self.xx:
-            self.assertTrue(np.isscalar(clenshaw(x, self.ak)))
-        self.assertFalse(np.isscalar(clenshaw(xx, self.ak)))
+    def test_clenshaw__scalar_input(self, evaluation_data):
+        xx = evaluation_data["xx"]
+        ak = evaluation_data["ak"]
+        for x in xx:
+            assert np.isscalar(clenshaw(x, ak))
+        assert not np.isscalar(clenshaw(xx, ak))
 
-    def test_bary__scalar_input(self):
-        for x in self.xx:
-            self.assertTrue(np.isscalar(bary(x, self.fk, self.xk, self.vk)))
-        self.assertFalse(np.isscalar(bary(xx, self.fk, self.xk, self.vk)))
+    def test_bary__scalar_input(self, evaluation_data):
+        xx = evaluation_data["xx"]
+        fk = evaluation_data["fk"]
+        xk = evaluation_data["xk"]
+        vk = evaluation_data["vk"]
+        for x in xx:
+            assert np.isscalar(bary(x, fk, xk, vk))
+        assert not np.isscalar(bary(xx, fk, xk, vk))
 
     # Check that we always get float output for constant Chebtechs, even
     # when passing in an integer input.
@@ -72,14 +81,14 @@ class Evaluation(unittest.TestCase):
     def test_bary__float_output(self):
         ff = Chebtech2.initconst(1)
         gg = Chebtech2.initconst(1.0)
-        self.assertTrue(isinstance(ff(0, "bary"), float))
-        self.assertTrue(isinstance(gg(0, "bary"), float))
+        assert isinstance(ff(0, "bary"), float)
+        assert isinstance(gg(0, "bary"), float)
 
     def test_clenshaw__float_output(self):
         ff = Chebtech2.initconst(1)
         gg = Chebtech2.initconst(1.0)
-        self.assertTrue(isinstance(ff(0, "clenshaw"), float))
-        self.assertTrue(isinstance(gg(0, "clenshaw"), float))
+        assert isinstance(ff(0, "clenshaw"), float)
+        assert isinstance(gg(0, "clenshaw"), float)
 
     # Check that we get consistent output from bary and clenshaw
     # TODO: Move these tests elsewhere?
@@ -91,15 +100,28 @@ class Evaluation(unittest.TestCase):
             for xx in evalpts:
                 fb = ff(xx, "bary")
                 fc = ff(xx, "clenshaw")
-                self.assertEquals(type(fb), type(fc))
+                assert type(fb) == type(fc)
 
 
 evalpts = [np.linspace(-1, 1, int(n)) for n in np.array([1e2, 1e3, 1e4, 1e5])]
 ptsarry = [Chebtech2._chebpts(n) for n in np.array([100, 200])]
 methods = [bary, clenshaw]
 
+# Generate test parameters
+test_params = []
+for method in methods:
+    for fun, _, _ in testfunctions:
+        for j, chebpts in enumerate(ptsarry):
+            for k, xx in enumerate(evalpts):
+                test_params.append((method, fun, xx, chebpts, j, k))
 
-def evalTester(method, fun, evalpts, chebpts):
+# Define the test function with parametrization
+@pytest.mark.parametrize(
+    "method,fun,evalpts,chebpts,j,k",
+    test_params,
+    ids=lambda param: f"{param.__name__}" if hasattr(param, "__name__") else str(param)
+)
+def test_eval_methods(method, fun, evalpts, chebpts, j, k):
     x = evalpts
     xk = chebpts
     fvals = fun(xk)
@@ -118,40 +140,35 @@ def evalTester(method, fun, evalpts, chebpts):
     n = evalpts.size
     tol = tol_multiplier * scaled_tol(n)
 
-    return infNormLessThanTol(a, b, tol)
+    assert infnorm(a - b) <= tol
 
 
-for method in methods:
-    for fun, _, _ in testfunctions:
-        for j, chebpts in enumerate(ptsarry):
-            for k, xx in enumerate(evalpts):
-                testfun = evalTester(method, fun, xx, chebpts)
-                testfun.__name__ = "test_{}_{}_{:02}_{:02}".format(
-                    method.__name__, fun.__name__, j, k
-                )
-                setattr(Evaluation, testfun.__name__, testfun)
+@pytest.fixture
+def coeffmult_data():
+    return {
+        "f": lambda x: exp(x),
+        "g": lambda x: cos(x),
+        "fn": 15,
+        "gn": 15
+    }
 
+class TestCoeffMult:
+    def test_coeffmult(self, coeffmult_data):
+        f = coeffmult_data["f"]
+        g = coeffmult_data["g"]
+        fn = coeffmult_data["fn"]
+        gn = coeffmult_data["gn"]
 
-class CoeffMult(unittest.TestCase):
-    def setUp(self):
-        self.f = lambda x: exp(x)
-        self.g = lambda x: cos(x)
-        self.fn = 15
-        self.gn = 15
-
-    def test_coeffmult(self):
         def h(x):
-            return self.f(x) * self.g(x)
+            return f(x) * g(x)
 
-        f, g = self.f, self.g
-        fn, gn = self.fn, self.gn
         hn = fn + gn - 1
         fc = Chebtech2.initfun(f, fn).prolong(hn).coeffs
         gc = Chebtech2.initfun(g, gn).prolong(hn).coeffs
         hc = coeffmult(fc, gc)
         HC = Chebtech2.initfun(h, hn).coeffs
-        self.assertLessEqual(infnorm(hc - HC), 2e1 * eps)
+        assert infnorm(hc - HC) <= 2e1 * eps
 
 
-# reset the testsfun variable so it doesn't get picked up by nose
+# reset the testfun variable to avoid any potential issues
 testfun = None
